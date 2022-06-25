726d47b10f4aec97c029dff8500c0c82437b3d29
its-me-bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2022-06-25 21:44:54 +0530

    sum of even numbers till N

---
+++ sum_of_even_num_tillN.cpp
@@ -0,0 +1,27 @@
+/*Given a number N, print sum of all even numbers from 1 to N.
+Input Format :
+Integer N
+Output Format :
+Required Sum 
+Sample Input 1 :
+ 6
+Sample Output 1 :
+12*/
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    cin >> n;
+    int i = 0 ;
+    int sum = 0 ;
+
+    while( i <= n){
+        sum = sum + i ;
+        i = i + 2 ;
+    }
+    cout << sum << endl;
+}
+
