d357ead82cc110ff4bf1c94b4db9410d456f4700
its-me-bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2022-07-02 15:44:40 +0530

    sum of two numbers

---
+++ CODING_NINJAS_C /add_two_numbers.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+
+    //sum of two interger number
+    int a , b ;
+    printf("Enter two number :\n");
+    scanf("%d%d" , &a , &b);
+    int sum = a + b ;
+    printf("%d\n" , sum);
+    return sum ;
+    }
+
+
+//sum of two decimal numbers (float values)
+/*
+    float c , d ;
+printf("Enter c ,d :");
+scanf("%f%f" , &c,&d);
+float plus = c + d;
+printf("%f\n",plus);
+return plus;*/
+
+
+
+
