94569ce0bdce57bfa7e6f74f49387d359054af7c
its-me-bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2022-07-03 14:01:51 +0530

    changes occurs

--- CODING_NINJAS_C++/pattern.cpp
+++ CODING_NINJAS_C++/pattern.cpp
@@ -1,8 +1,45 @@
+/*Number Pattern 1
+
+Print the following pattern
+Pattern for N = 4
+1
+23
+345
+4567
+Input Format :
+N (Total no. of rows)
+Output Format :
+Pattern in N lines
+Sample Input 1 :
+3
+Sample Output 1 :
+1
+23
+345*/
+
+
+
+
+
 #include<iostream>
 using namespace std;
 
 int main  (){
     int n ;
-    cout << "Enter n" << endl;
+    cout << "Enter num" << endl;
+cin >> n ;
+
+int i = 1 ;
+while(i <= n ){
+    int j = 1 ;
+    int var = i ;
+    while( j <= i ){
+        cout << var ;
+        j++;
+        var++;
+    }
+    cout << endl;
+    i++;
+}
     
 }
