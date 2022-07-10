0d9bca01c442a6a085e7ce6ec3763fcc02d13424
its-me-bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2022-07-10 20:41:50 +0530

    area

---
+++ CODING_NINJAS_C /area_of_rectangle.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int lenght , breadth ;
+    printf("Enter length and breadth : \n");
+    scanf("%d%d" , &lenght,&breadth);
+
+    printf("Area of reactangle = %d\n" , lenght*breadth);
+    
+
+}
