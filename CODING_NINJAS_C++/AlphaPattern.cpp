38ad58e0d1e6779dce135548e1e2d947852c0468
its-me-bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2022-07-24 14:17:40 +0530

    alpha pattern

---
+++ CODING_NINJAS_C++/AlphaPattern.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    
+    /*  Read input as specified in the question.
+     * Print output as specified in the question.
+     */
+       int n = 3;
+    cin >> n;
+    int i = 1;
+    char ch = 'A';
+    while (i <= n){
+        int j = 1;
+        while (j < i + 1){
+            cout << ch;
+            j++;
+        }
+        ch++;
+        cout <<endl;
+        i++;
+    }
+}
+
+
+
+/*Code : Alpha Pattern
+Send Feedback
+Print the following pattern for the given N number of rows.
+Pattern for N = 3
+ A
+ BB
+ CCC
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 26
+Sample Input 1:
+7
+Sample Output 1:
+A
+BB
+CCC
+DDDD
+EEEEE
+FFFFFF
+GGGGGGG
+Sample Input 2:
+6
+Sample Output 2:
+A
+BB
+CCC
+DDDD
+EEEEE
+FFFFFF
+
+
+*/
